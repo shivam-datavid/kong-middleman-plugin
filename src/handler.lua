8c329e82e52cc847b830efacf8d708b9c1ae4e60
Shivam Singh <103422551+shivam-datavid@users.noreply.github.com>
2024-04-19 11:22:13 +0530

    Update handler.lua

--- src/handler.lua
+++ src/handler.lua
@@ -1,16 +1,12 @@
-local BasePlugin = require "kong.plugins.base_plugin"
 local access = require "kong.plugins.middleman.access"
+local kong_meta = require "kong.meta"
 
-local MiddlemanHandler = BasePlugin:extend()
-
-MiddlemanHandler.PRIORITY = 900
-
-function MiddlemanHandler:new()
-  MiddlemanHandler.super.new(self, "middleman")
-end
+local MiddlemanHandler = {
+  VERSION = kong_meta.version,
+  PRIORITY = 1100,
+}
 
 function MiddlemanHandler:access(conf)
-  MiddlemanHandler.super.access(self)
   access.execute(conf)
 end
 
